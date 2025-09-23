e0b29bd684fb819cf1ad0368cc1c11e9f5b48e11
quang1606 <quangnguyenxuanymnb2004@gmail.com>
2025-09-23 19:29:36 +0700

    sprint 1

--- Backend/src/main/java/com/example/backend/repository/SachRepository.java
+++ Backend/src/main/java/com/example/backend/repository/SachRepository.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SachRepository extends JpaRepository<Sach, Integer>, JpaSpecificationExecutor<Sach> {
-    @Query("select s from Sach  s "+
-            "where lower(s.tenSach) like lower(concat('%',:keyword,'%') ) ")
+    @Query("SELECT s FROM Sach s " +
+            "JOIN s.tacGia tg " +
+            "WHERE lower(tg.tenTacGia) LIKE lower(concat('%', :keyword, '%'))" +
+                "or lower(s.tenSach) like lower(concat('%', :keyword, '%') ) ")
     Page<Sach> searchByKeyWord(@Param("keyword") String keyword, Pageable pageable);
 }
