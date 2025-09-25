9e95130633ea5dd5b2cfd82825aaa53402c8b494
jinrecatdo <139969873+jinrecatdo@users.noreply.github.com>
2025-09-25 13:45:11 +0700

    update duong dan

--- src/pages/BookList/BookList.jsx
+++ src/pages/BookList/BookList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import BookCard from "../BookCard/BookCard";
+import BookCard from "../../Components/BookCard/BookCard";
 import "./BookList.css";
 
 export default function BookList() {
