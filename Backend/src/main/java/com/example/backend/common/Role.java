1b9e66a8e691e9f2ca94fcc6ef5ec00be43222ab
quang1606 <167396403+quang1606@users.noreply.github.com>
2025-10-03 23:15:26 +0700

    Revert "Backend"

--- Backend/src/main/java/com/example/backend/common/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.common;
-
-public enum Role {
-    USER, ADMIN
-}
--- Backend/src/main/java/com/example/backend/repository/GioHangRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.GioHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GioHangRepository extends JpaRepository<GioHang, Integer> {
-}
